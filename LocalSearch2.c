95693ff4e18f6547d6491c0210d4a8e8caa2aee3
taiga2010 <taiga.osu@gmail.com>
2018-11-23 17:32:15 +0900

    フォーマット修正

--- LocalSearch2.c
+++ LocalSearch2.c
@@ -18,9 +18,7 @@ int check_drop(int n, Dictionary *Dict, Tuple *T, int sol_size, int **cover, int
 void kick(int n, Dictionary *Dict, Tuple *T, int t_size, int *Sol, int *sol_size, int **puzzle, int **enable, int *score, int **cover);
 void shuffle(int arr[], int size);
 void display(Dictionary *Dict, int **puzzle, int *score, int sol_size, int n);
-int move(int n, Dictionary *Dict,
-    Tuple *T, int t_size, int *Sol, int ****InvT,
-    int **puzzle, int *score, int sol_size, int **enable, int **cover);
+int move(int n, Dictionary *Dict, Tuple *T, int t_size, int *Sol, int ****InvT, int **puzzle, int *score, int sol_size, int **enable, int **cover);
 int check_move(int n, int **puzzle, int **enable, int **cover, int direction);
 int calc_profit(Dictionary *Dict, Tuple *T, int t_size, int *Sol);
 void breakConnection(int n, Dictionary *Dict, Tuple *T, int t_size, int *Sol, int *sol_size, int t, int **puzzle, int **enable, int *score, int **cover);
@@ -382,15 +380,14 @@ int add(int n, Dictionary *Dict, Tuple *T, int t_size, int *Sol, int sol_size, i
   }
 
   //cover配列の表示
-  /*for (p = 0; p < n; p++) {
-      for (q = 0; q < n; q++) {
-        printf("%d ", cover[p][q]);
-      }
-      printf("\n");
+  /*
+  for (p = 0; p < n; p++) {
+    for (q = 0; q < n; q++) {
+      printf("%d ", cover[p][q]);
     }
+    printf("\n");
+  }
   */
-
-
 
   //カバー配列の表示
  /*
@@ -433,8 +430,8 @@ int calc(int n, Dictionary *Dict, int **puzzle, int **cover, int mode) {
       for (q = 0; q < n; q++) {
         score += cover[p][q];
       }
-      return score;
     }
+    return score;
   }
 
   int *word;
