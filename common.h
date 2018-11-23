75769ca424a97f574e1deb1d472b63cced4fa590
Saikoro2007 <saikorokorokoro20072007@gmail.com>
2018-11-23 23:05:23 +0900

    Revert "グローバル変数追加・引数整理"
    This reverts commit f5099ab64abcd9873a1d5d6e4cc7e766dade273e.

--- common.h
+++ common.h
@@ -5,19 +5,17 @@ int n;          // 盤面の次数
 Tuple *T;       // すべての可能なタプル集合
 int *Sol;       // 解であるタプルの添字集合
 int t_size;     // T に属するタプルの個数
-int **puzzle;
-int **enable;
-int **cover;
+
 /***** subroutines *****/
-int add(int sol_size, int t, int *score);
-int drop(int sol_size, int *score, int t);
-int calc(int mode);
-int check_add(int sol_size, int t);
-int check_drop(int sol_size, int t);
-void kick(int *sol_size, int *score);
+int add(int sol_size, int t, int **puzzle, int **enable, int *score, int **cover);
+int drop(int sol_size, int **puzzle, int **enable, int *score, int **cover, int t);
+int calc(int **puzzle, int **cover, int mode);
+int check_add(int sol_size, int t, int **puzzle, int **enable);
+int check_drop(int sol_size, int **cover, int t);
+void kick(int *sol_size, int **puzzle, int **enable, int *score, int **cover);
 void shuffle(int arr[], int size);
-void display(int *score, int sol_size);
-int move(int ****InvT, int *score, int sol_size);
-int check_move(int direction);
+void display(int **puzzle, int *score, int sol_size);
+int move(int ****InvT, int **puzzle, int *score, int sol_size, int **enable, int **cover);
+int check_move(int **puzzle, int **enable, int **cover, int direction);
 int calc_profit();
-void breakConnection(int *sol_size, int t, int *score);
+void breakConnection(int *sol_size, int t, int **puzzle, int **enable, int *score, int **cover);
