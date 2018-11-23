ccc01408d2927a0f85124a1652bba1d3eb6d694e
Saikoro2007 <saikorokorokoro20072007@gmail.com>
2018-11-23 23:05:12 +0900

    Revert "Revert "微修正""
    This reverts commit 79b1ef337f47d6486e5fdf0a6b77bbd281d1fd50.

--- common.h
+++ common.h
@@ -14,7 +14,7 @@ int check_add(int sol_size, int t, int **puzzle, int **enable);
 int check_drop(int sol_size, int **cover, int t);
 void kick(int *sol_size, int **puzzle, int **enable, int *score, int **cover);
 void shuffle(int arr[], int size);
-void display(int **puzzle, int *score, int sol_size, int n);
+void display(int **puzzle, int *score, int sol_size);
 int move(int ****InvT, int **puzzle, int *score, int sol_size, int **enable, int **cover);
 int check_move(int **puzzle, int **enable, int **cover, int direction);
 int calc_profit();
