bdf03dc8f36a111e766fc8569218b9429f03392d
Saikoro2007 <saikorokorokoro20072007@gmail.com>
2018-11-23 23:04:54 +0900

    Revert "Revert "Revert "グローバル変数追加・引数整理"""
    This reverts commit 1a017c8bb10a3792c9e92ca409c8e208751df6c0.

--- main.c
+++ main.c
@@ -91,6 +91,8 @@ int main(int argc, char *argv[]) {
   t_size = t;
 
   //パズルの盤面と禁止マスの準備
+  int **puzzle;
+  int **enable;
   int **tmp_puzzle;
   int **tmp_cover;
   int **tmp_enable;
@@ -117,6 +119,7 @@ int main(int argc, char *argv[]) {
   }
 
   //cover配列の準備
+  int **cover;
   cover = (int**)malloc(n * sizeof(int*));
   for (p = 0; p < n; p++) {
     cover[p] = (int*)malloc(n * sizeof(int));
@@ -148,7 +151,7 @@ int main(int argc, char *argv[]) {
   while (tmp != sol_size) {
     tmp = sol_size;
     for (t = 0; t < t_size; t++) {
-      sol_size = add(sol_size, arr_rand[t], &score);
+      sol_size = add(sol_size, arr_rand[t], puzzle, enable, &score, cover);
     }
   }
   profit = calc_profit();
@@ -162,7 +165,7 @@ int main(int argc, char *argv[]) {
   }
 
   clock_t now = clock();//現在時間
-  display(&score, sol_size);
+  display(puzzle, &score, sol_size);
   printf("white rate:%f\n", 100 * white / (n*n));
   printf("Processing time : %f[s]\n", (double)(now - start) / CLOCKS_PER_SEC);
 
@@ -201,14 +204,14 @@ int main(int argc, char *argv[]) {
     }
 
     //連結性が崩れるまでdrop
-    breakConnection(&sol_size, t, &score);
+    breakConnection(&sol_size, t, puzzle, enable, &score, cover);
 
     //kickする
-    kick(&sol_size, &score);
+    kick(&sol_size, puzzle, enable, &score, cover);
 
     //移動できる方向をランダムで選び、平行移動する
     if (movetype==1) {
-      if (move(InvT, &score, sol_size) == True) {
+      if (move(InvT, puzzle, &score, sol_size, enable, cover) == True) {
         moveCount++;
       }
     }
@@ -219,7 +222,7 @@ int main(int argc, char *argv[]) {
     while (tmp != sol_size) {
       tmp = sol_size;
       for (t = 0; t < t_size; t++) {
-        sol_size = add(sol_size, arr_rand[t], &score);
+        sol_size = add(sol_size, arr_rand[t], puzzle, enable, &score, cover);
       }
     }
 
@@ -242,7 +245,7 @@ int main(int argc, char *argv[]) {
         improveCount++;
       }
       printf("--- improved[%d] ---\n\n", improveCount);
-      display(&score, sol_size);
+      display(puzzle, &score, sol_size);
       now = clock();//現在時間
       printf("white rate:%f\n", 100 * white / (n*n));
       printf("Processing time : %f[s]\n", (double)(now - start) / CLOCKS_PER_SEC);
@@ -269,7 +272,7 @@ int main(int argc, char *argv[]) {
 
   //最終的なpuzzleの表示
   printf("--- COMPLETE ---\n\n");
-  display(&score, sol_size);
+  display(puzzle, &score, sol_size);
   printf("profit:%d\n", profit);
   now = clock();//現在時間
   printf("Processing time:%f[s]\n", (double)(now - start) / CLOCKS_PER_SEC);
